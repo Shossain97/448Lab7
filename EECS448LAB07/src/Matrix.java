5ac6436fe5ae8c31e3dc7a31fb20a4d5d5b5e8b5
shossain97 <shafeen.hossain97@gmail.com>
2017-03-31 08:43:28 -0500

    Matrix private variables and constructor created

---
+++ EECS448LAB07/src/Matrix.java
@@ -0,0 +1,14 @@
+
+public class Matrix {
+	//Max size of matrix
+	public static final int MaxSize=10;
+	//size of current matrix
+	private int n;
+	private double[][] mData=new double[MaxSize][MaxSize];
+	public Matrix(int _n)
+	{
+		this.n=_n;
+	}
+	
+
+}
