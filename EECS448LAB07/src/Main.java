9e1a5275c0aff298f6c5cc39b1b3a173d5c4a933
shossain97 <shafeen.hossain97@gmail.com>
2017-03-30 22:57:44 -0500

    Deleted main cause not working

--- EECS448LAB07/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-public class Main {
-
-	public static void main(int argc, String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("This is a line of code");
-	}
-
-}
