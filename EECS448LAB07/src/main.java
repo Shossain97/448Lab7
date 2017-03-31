54bdcdb9a69cd106adf4f3158b760e73db4b22fa
shossain97 <shafeen.hossain97@gmail.com>
2017-03-31 08:22:17 -0500

    File reader and files

--- EECS448LAB07/src/main.java
+++ EECS448LAB07/src/main.java
@@ -1,13 +1,38 @@
 import java.io.*;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a line");
-		if(args.length<3)
+		if(args.length<2)
 		{
 			throw new RuntimeException("Expecting a format of ./main input.txt output.txt");
 		}
+		System.out.println(args.length);
+		//File stream
+		try
+		{
+			BufferedReader inFile = new BufferedReader(new FileReader(new File(args[0])));
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("File not found");
+		}
+		catch(IOException e)
+		{
+			System.out.println("Cannot read file");
+		}
+		try
+		{
+			BufferedWriter outFile=new BufferedWriter(new FileWriter(new File(args[1])));
+		}
+		catch(IOException e)
+		{
+			System.out.println("Cannot write to file");
+		}
+		
+
 	}
 
 }
