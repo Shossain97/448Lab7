8633103e6244ae95c3ee062ef203ff503fd579bc
shossain97 <shafeen.hossain97@gmail.com>
2017-03-31 08:56:10 -0500

    Created part of determinant requires submatrix so will make that first then come back to this

--- EECS448LAB07/src/Matrix.java
+++ EECS448LAB07/src/Matrix.java
@@ -4,10 +4,29 @@ public class Matrix {
 	public static final int MaxSize=10;
 	//size of current matrix
 	private int n;
-	private double[][] mData=new double[MaxSize][MaxSize];
+	private double[][] mdata=new double[MaxSize][MaxSize];
 	public Matrix(int _n)
 	{
 		this.n=_n;
+	}
+	//Determinant Calculation
+	public double determinant()
+	{
+		//Determinant
+		double det=0.0;
+		if(n==1)
+		{
+			det=mdata[0][0];
+		}
+		else if(n==2)
+		{
+			det = mdata[0][0] * mdata[1][1] - mdata[0][1] * mdata[1][0];
+		}
+		else
+		{
+			//Code that rerquires submatrix to be made
+		}
+		return det;
 	}
 	
 
