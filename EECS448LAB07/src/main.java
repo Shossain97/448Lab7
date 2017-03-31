ec92da2b027ab7c05936f20836febdd5f3a69415
shossain97 <shafeen.hossain97@gmail.com>
2017-03-31 08:01:13 -0500

    Error checking made to work

--- EECS448LAB07/src/main.java
+++ EECS448LAB07/src/main.java
@@ -4,6 +4,10 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a line");
+		if(args.length<3)
+		{
+			throw new RuntimeException("Expecting a format of ./main input.txt output.txt");
+		}
 	}
 
 }
