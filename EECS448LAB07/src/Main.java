57f87beff2e7d32e3dd250339400c5ac32c613ac
shossain97 <shafeen.hossain97@gmail.com>
2017-03-30 22:55:35 -0500

    Importing library testing if the eclipse git is working

--- EECS448LAB07/src/Main.java
+++ EECS448LAB07/src/Main.java
@@ -1,9 +1,9 @@
-
+import java.io.*;
 public class Main {
 
 	public static void main(int argc, String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("This is a line of code");
 	}
 
 }
