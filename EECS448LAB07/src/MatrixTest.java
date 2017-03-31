73d06dd3c1b4331d0c0931d4c6011d754a69e6a3
shossain97 <shafeen.hossain97@gmail.com>
2017-03-31 14:08:45 -0500

    Test submatrix created and is successful

--- EECS448LAB07/src/MatrixTest.java
+++ EECS448LAB07/src/MatrixTest.java
@@ -28,8 +28,27 @@ public class MatrixTest {
 
 	@Test
 	public void testSubMatrix() {
+		Matrix m=new Matrix(3);
+		m.matrixSet(0,0,8);
+		m.matrixSet(0,1,9);
+		m.matrixSet(0,2,1);
 		
-		fail("Not yet implemented");
+		m.matrixSet(1,0,3);
+		m.matrixSet(1,1,5);
+		m.matrixSet(1,2,2);
+		
+		m.matrixSet(2,0,-2);
+		m.matrixSet(2,1,3);
+		m.matrixSet(2,2,-1);
+		
+		Matrix sub=new Matrix(2);
+		sub.matrixSet(0,0,5);
+		sub.matrixSet(0,1,2);
+		
+		sub.matrixSet(1, 0, 3);
+		sub.matrixSet(1, 1, -1);
+		assertEquals(sub.toString(),m.subMatrix(0,0).toString());
+		
 	}
 
 	@Test
@@ -37,9 +56,6 @@ public class MatrixTest {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
+
 
 }
